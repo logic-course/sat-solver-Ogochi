cfa841c7137b449e46077ef96864e364f4e3c3d5
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2020-05-30 01:20:21 +0200

    Added negation propagation to cnf converter

--- src/ogochi/sat/CnfConverter.java
+++ src/ogochi/sat/CnfConverter.java
@@ -3,6 +3,64 @@ package ogochi.sat;
 public class CnfConverter {
   
   public Formula Convert(Formula formula) {
-    return null;
+
+    System.out.println(NegationForm(formula, false));
+    return formula;
+  }
+
+  private Formula NegationForm(Formula formula, boolean shallNegate) {
+    if (formula.type == FormulaType.Not) {
+        return NegationForm(formula.val1, !shallNegate);
+    }
+
+    if (shallNegate) {
+      switch (formula.type) {
+        case And:
+        return new Formula(FormulaType.Or, NegationForm(formula.val1, true), NegationForm(formula.val2, true));
+        case F:
+          return new Formula(FormulaType.T);
+        case Iff:
+          return NegationForm(
+            new Formula(
+              FormulaType.And,
+              new Formula(FormulaType.Imp, formula.val1, formula.val2),
+              new Formula(FormulaType.Imp, formula.val2, formula.val1)),
+            true);
+        case Imp:
+          return new Formula(FormulaType.Or, NegationForm(formula.val1, true), NegationForm(formula.val2, false));
+        case Or:
+          return new Formula(FormulaType.And, NegationForm(formula.val1, true), NegationForm(formula.val2, true));
+        case T:
+          return new Formula(FormulaType.F);
+        case Var:
+          return new Formula(formula.varName, true);
+      }
+    }
+
+    switch (formula.type) {
+      case And:
+        formula.val1 = NegationForm(formula.val1, false);
+        formula.val2 = NegationForm(formula.val2, false);
+        break;
+      case Iff:
+        formula.val1 = NegationForm(formula.val1, false);
+        formula.val2 = NegationForm(formula.val2, false);
+        break;
+      case Imp:
+        formula.val1 = NegationForm(formula.val1, false);
+        formula.val2 = NegationForm(formula.val2, false);
+        break;
+      case Not:
+        formula.val1 = NegationForm(formula.val1, false);
+        break;
+      case Or:
+        formula.val1 = NegationForm(formula.val1, false);
+        formula.val2 = NegationForm(formula.val2, false);
+        break;
+      default:
+        break;
+    }
+
+    return formula;
   }
 }
--- src/ogochi/sat/Formula.java
+++ src/ogochi/sat/Formula.java
@@ -4,9 +4,41 @@ public class Formula {
   FormulaType type;
   String varName;
   Formula val1, val2;
+  boolean negated = false;
+
+  public Formula () {}
+
+  public Formula (FormulaType type, String varName, Formula val1, Formula val2) {
+    this.type = type;
+    this.varName = varName;
+    this.val1 = val1;
+    this.val2 = val2;
+  }
+
+  public Formula (FormulaType type) {
+    this.type = type;
+  }
+
+  public Formula (String varName) {
+    this(FormulaType.Var, varName, null, null);
+  }
+
+  public Formula (String varName, boolean negated) {
+    this(varName);
+    this.negated = negated;
+  }
+
+  public Formula (FormulaType type, Formula val1) {
+    this(type, null, val1, null);
+  }
+
+  public Formula (FormulaType type, Formula val1, Formula val2) {
+    this(type, null, val1, val2);
+  }
 
   public String toString() {
-    return type.name() + (varName != null ? "'" + varName + "'" : "") + 
+    return (negated ? "~" : "") + type.name() +
+      (varName != null ? "'" + varName + "'" : "") + 
       (val1 != null ? " (" + (val1 == null ? "" : val1) + " , " + (val2 == null ? "" : val2) + " )" : "");
   }
 }
